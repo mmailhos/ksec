6be6fc68cca365700c1385e4fc16df6b47eade75
Mathieu Mailhos <mathieumailhos@gmail.com>
2019-10-22 14:02:08 +1100

    feat: add multiple outputs
    Add YAML|JSON|Env outputs.
    TODO: metadata and color need a rethink after that feature

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
-	"github.com/fatih/color"
-	"k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
-	"k8s.io/client-go/tools/clientcmd"
 	"os"
 	"regexp"
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/fatih/color"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // kubeAPI returns a CoreV1Interface object to call Kubernetes API
@@ -32,23 +34,6 @@ func kubeAPI(kubeconfig clientcmd.ClientConfig) corev1.CoreV1Interface {
 	return clientset.CoreV1()
 }
 
-// printResult do the final logic taken an array of secrets
-func printResult(foundSecrets []*v1.Secret, flagColor, flagMetadata bool, targetData string) {
-	switch len(foundSecrets) {
-	case 0:
-		fmt.Println("No secret found")
-	case 1:
-		for _, secret := range foundSecrets {
-			printSecret(secret, flagColor, flagMetadata, targetData)
-		}
-	default:
-		fmt.Println("Unable to determine the target. Try one of these:")
-		for _, secret := range foundSecrets {
-			fmt.Println(secret.Name)
-		}
-	}
-}
-
 // secretData. Handy struct to convert secret data into a Slice to sort later
 type secretDataType struct {
 	Key   string
@@ -61,15 +46,15 @@ func prExit(err error) {
 	os.Exit(1)
 }
 
-// sortedData will take secretData input and return a sorted array of keys
-func sortedData(secretData map[string][]byte, targetData string) (secData []secretDataType) {
+// sortFiler will take secretData input and return a sorted array of keys
+func sortFiler(secretData map[string][]byte, targetData string) (secData []secretDataType) {
 	for k, v := range secretData {
 		if targetData == "" || strings.Contains(strings.ToLower(k), strings.ToLower(targetData)) {
 			secData = append(secData, secretDataType{Key: k, Value: string(v)})
 		}
 	}
 	if len(secData) == 0 {
-		fmt.Printf("No data key found with %s\n", targetData)
+		fmt.Fprintf(os.Stderr, "No data key found with %s\n", targetData)
 	}
 	sort.Slice(secData, func(i, j int) bool {
 		return secData[i].Key < secData[j].Key
@@ -77,20 +62,33 @@ func sortedData(secretData map[string][]byte, targetData string) (secData []secr
 	return
 }
 
-// printSecret simply prints on STDOUT the data of a given secret
-func printSecret(secret *v1.Secret, flagColor, flagMetadata bool, targetData string) {
+// printSecretSeparator simply prints on STDOUT the data of a given secret with KeySeparatorValue format.
+func printSecretSeparator(secret *v1.Secret, flagColor, flagMetadata bool, targetData, sep string) {
 	if flagMetadata {
-		fmt.Printf("Name: %s, Type: %s, Count: %v, Size: %v\n", secret.Name, secret.Type, len(secret.Data), secret.Size())
+		fmt.Printf("# Name: %s, Type: %s, Count: %v, Size: %v\n", secret.Name, secret.Type, len(secret.Data), secret.Size())
 	}
-	for _, sd := range sortedData(secret.Data, targetData) {
+	for _, sd := range sortFiler(secret.Data, targetData) {
 		if flagColor {
 			color.New(color.FgBlue).Printf(sd.Key)
-			fmt.Printf(": ")
+			fmt.Printf(sep)
 			color.New(color.FgGreen).Println(sd.Value)
 		} else {
-			fmt.Printf("%s: %s\n", sd.Key, sd.Value)
+			fmt.Printf("%s%s%s\n", sd.Key, sep, sd.Value)
 		}
 	}
+}
+
+// printSecretJSON do the final logic taken an array of secrets
+func printSecretJSON(secret *v1.Secret, targetData string) {
+	secs := make(map[string]string)
+	for _, sd := range sortFiler(secret.Data, targetData) {
+		secs[sd.Key] = sd.Value
+	}
+	secretJSON, err := json.Marshal(secs)
+	if err != nil {
+		prExit(err)
+	}
+	fmt.Fprintf(os.Stdout, "%s", secretJSON)
 }
 
 // getRelease returns the release number (or 0) of a given secret name
@@ -133,7 +131,7 @@ func getSecrets(targetSecret, targetType string, secrets *v1.SecretList) (foundS
 }
 
 func main() {
-	var flagNamespace, flagLabel, flagField, flagType, namespace, targetData string
+	var flagNamespace, flagLabel, flagField, flagType, flagOut, namespace, targetData string
 	var flagColor, flagMetadata bool
 	var err error
 
@@ -141,6 +139,7 @@ func main() {
 	flag.StringVar(&flagLabel, "label", "", "Label selector")
 	flag.StringVar(&flagField, "field", "", "Field selector")
 	flag.BoolVar(&flagColor, "color", false, "Use colors")
+	flag.StringVar(&flagOut, "out", "env", "Output format (env, json, yaml)")
 	flag.BoolVar(&flagMetadata, "metadata", false, "Print metadata of the found secret (Name, Type)")
 	flag.StringVar(&flagType, "type", "", "Look for a specific secret type (ex: Opaque)")
 	flag.Parse()
@@ -167,10 +166,22 @@ func main() {
 
 	api := kubeAPI(kubeconfig)
 
+	outSec := func(sec *v1.Secret) {
+		if flagOut == "yaml" || flagOut == "yml" {
+			printSecretSeparator(sec, flagColor, flagMetadata, targetData, ": ")
+		} else if flagOut == "env" {
+			printSecretSeparator(sec, flagColor, flagMetadata, targetData, "=")
+		} else if flagOut == "json" {
+			printSecretJSON(sec, targetData)
+		} else {
+			fmt.Fprintf(os.Stderr, "Unknown output format")
+		}
+	}
+
 	// Attempt to directly get the secret (e.g. perfect match) to avoid unnecessary operations
 	foundSecret, err := api.Secrets(namespace).Get(targetSecret, metav1.GetOptions{})
 	if err == nil && (flagType == "" || flagType == string(foundSecret.Type)) {
-		printSecret(foundSecret, flagColor, flagMetadata, targetData)
+		outSec(foundSecret)
 		os.Exit(0)
 	}
 
@@ -180,5 +191,17 @@ func main() {
 	if err != nil {
 		prExit(err)
 	}
-	printResult(getSecrets(targetSecret, flagType, secrets), flagColor, flagMetadata, targetData)
+
+	foundSecrets := getSecrets(targetSecret, flagType, secrets)
+	switch len(foundSecrets) {
+	case 0:
+		fmt.Println("No secret found")
+	case 1:
+		outSec(foundSecrets[0])
+	default:
+		fmt.Println("Unable to determine the target. Try one of these:")
+		for _, secret := range foundSecrets {
+			fmt.Println(secret.Name)
+		}
+	}
 }
